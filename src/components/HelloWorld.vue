729ff3dc75a1c09c97bd3fb1de7d8fdd6b6df293
softweredevloper <ssayam200@gmail.com>
2023-06-01 21:13:53 +0530

    commit done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,30 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
+
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit this 
+      <code>components/HelloWorld.vue</code> to test the website
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+
+  <p>
+    Install
+    <a href="
+    https://github.com/vuejs/vite-plugin-vue2-jsx" target="_blank"
+      >Volar</a
+    >, the official Vue + Vite starter
+  </p>
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
