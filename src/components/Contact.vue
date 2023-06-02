2ad6ac9f5a1926abbaa96e4a5c4dd31828c4153a
softweredevloper <ssayam200@gmail.com>
2023-06-02 18:55:19 +0530

    style to contact page addedd

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -7,5 +7,20 @@
 </template>
 
 <style scoped>
+.read-the-docs {
+  color: #888;
+}
+.read-the-docs:hover {
+  color: #000;
+}
+.card {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 20px;
+  margin-top: 20px;
+}
 
+.card p {
+  margin: 0;
+}
 </style>
