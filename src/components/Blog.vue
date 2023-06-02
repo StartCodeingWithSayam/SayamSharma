d61af39f2a36783e069a4c2470683e3beb0fbff6
softweredevloper <ssayam200@gmail.com>
2023-06-02 18:52:06 +0530

    blog page added

---
+++ src/components/Blog.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Blog</h1>
+    <p>
+      Edit this 
+      <code>components/Blog.vue</code> to test the website
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
