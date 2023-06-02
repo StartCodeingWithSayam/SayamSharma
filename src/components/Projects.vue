8bace3c4e54c8838184f35915d64f54053b98fac
softweredevloper <ssayam200@gmail.com>
2023-06-02 18:53:18 +0530

    projects added

---
+++ src/components/Projects.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>Projects</h1>
+    <p>
+      Edit this 
+      <code>components/Projects.vue</code> to test the website
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
